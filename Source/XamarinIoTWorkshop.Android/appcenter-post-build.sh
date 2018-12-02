75e347e67958b6c173ccdf193092bc266dfae75f
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2018-12-02 22:48:55 +0100

    Updated Post Build Script

--- Source/XamarinIoTWorkshop.Android/appcenter-post-build.sh
+++ Source/XamarinIoTWorkshop.Android/appcenter-post-build.sh
@@ -15,15 +15,34 @@ chmod -R 777 $SolutionFileFolder
 msbuild "$UITestProject" /property:Configuration=$APPCENTER_XAMARIN_CONFIGURATION
 
 UITestDLL=`find "$APPCENTER_SOURCE_DIRECTORY" -name "XamarinIoTWorkshop.UITests.dll" | grep bin`
+echo UITestDLL: $UITestDLL
+
 UITestBuildDir=`dirname $UITestDLL`
+echo UITestBuildDir: $UITestBuildDir
+
+UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,10\}\-'dev`
+echo UITestPrereleaseVersionNumber: $UITestVersionNumber
+
+UITestVersionNumberSize=${#UITestVersionNumber} 
+echo UITestVersionNumberSize: $UITestVersionNumberSize
+
+if [ $UITestVersionNumberSize == 0 ]
+then
+    UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
+    echo UITestVersionNumber: $UITestVersionNumber
+fi
+
+TestCloudExe=`find ~/.nuget | grep test-cloud.exe | grep $UITestVersionNumber | head -1`
+echo TestCloudExe: $TestCloudExe
+
+TestCloudExeDirectory=`dirname $TestCloudExe`
+echo TestCloudExeDirectory: $TestCloudExeDirectory
 
 APKFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.apk | head -1`
-
-DSYMFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.dsym | head -1`
-DSYMDirectory=`dirname $DSYMFile`
+echo APKFile: $APKFile
 
 npm install -g appcenter-cli
 
 appcenter login --token token
 
-appcenter test run uitest --app "Xamarin-IoT-Workshop/XamarinIoTWorkshop-Android" --devices "Xamarin-IoT-Workshop/all-android-os" --app-path $APKFile --test-series "master" --locale "en_US" --build-dir $UITestBuildDir --dsym-dir $DSYMDirectory --async
+appcenter test run uitest --app "Xamarin-IoT-Workshop/XamarinIoTWorkshop-Android" --devices "Xamarin-IoT-Workshop/all-android-os" --app-path $APKFile --test-series "master" --locale "en_US" --build-dir $UITestBuildDir --uitest-tools-dir $TestCloudExeDirectory
--- Source/XamarinIoTWorkshop.iOS/appcenter-post-build.sh
+++ Source/XamarinIoTWorkshop.iOS/appcenter-post-build.sh
@@ -17,16 +17,38 @@ if [ "$APPCENTER_XAMARIN_CONFIGURATION" == "Debug" ];then
     msbuild "$UITestProject" /property:Configuration=$APPCENTER_XAMARIN_CONFIGURATION
 
     UITestDLL=`find "$APPCENTER_SOURCE_DIRECTORY" -name "XamarinIoTWorkshop.UITests.dll" | grep bin`
+    echo UITestDLL: $UITestDLL
+
     UITestBuildDir=`dirname $UITestDLL`
+    echo UITestBuildDir: $UITestBuildDir
+
+    UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,10\}\-'dev`
+    echo UITestPrereleaseVersionNumber: $UITestVersionNumber
+
+    UITestVersionNumberSize=${#UITestVersionNumber} 
+    echo UITestVersionNumberSize: $UITestVersionNumberSize
+
+    if [ $UITestVersionNumberSize == 0 ]
+    then
+        UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
+        echo UITestVersionNumber: $UITestVersionNumber
+    fi
+
+    TestCloudExe=`find ~/.nuget | grep test-cloud.exe | grep $UITestVersionNumber | head -1`
+    echo TestCloudExe: $TestCloudExe
+
+    TestCloudExeDirectory=`dirname $TestCloudExe`
+    echo TestCloudExeDirectory: $TestCloudExeDirectory
 
     IPAFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.ipa | head -1`
+    echo IPAFile: $IPAFile
 
-    DSYMFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.dsym | head -1`
-    DSYMDirectory=`dirname $DSYMFile`
+    DSYMFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.dSYM | head -1`
+    echo DSYMFile: $DSYMFile
 
     npm install -g appcenter-cli
 
     appcenter login --token token
 
-    appcenter test run uitest --app "Xamarin-IoT-Workshop/Xamarin-IoT-Workshop-iOS" --devices "Xamarin-IoT-Workshop/my-device-set" --app-path $IPAFile --test-series "master" --locale "en_US" --build-dir $UITestBuildDir --dsym-dir $DSYMDirectory --async
+    appcenter test run uitest --app "Xamarin-IoT-Workshop/Xamarin-IoT-Workshop-iOS" --devices "Xamarin-IoT-Workshop/my-device-set" --app-path $IPAFile --test-series "master" --locale "en_US" --build-dir $UITestBuildDir --dsym-dir $DSYMDirectory --uitest-tools-dir $TestCloudExeDirectory
 fi
