150bac24f13eb31ca424b1712dc733e9c5f50253
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-01-06 12:42:49 -0800

    Omit Ref Directory

--- Source/XamarinIoTWorkshop.iOS/appcenter-post-build.sh
+++ Source/XamarinIoTWorkshop.iOS/appcenter-post-build.sh
@@ -15,7 +15,7 @@ chmod -R 777 $SolutionFileFolder
 
 msbuild "$UITestProject" /property:Configuration=$APPCENTER_XAMARIN_CONFIGURATION
 
-UITestDLL=`find "$APPCENTER_SOURCE_DIRECTORY" -name "XamarinIoTWorkshop.UITests.dll" | grep bin | head -1`
+UITestDLL=`find "$APPCENTER_SOURCE_DIRECTORY" -name "XamarinIoTWorkshop.UITests.dll" | grep bin  | grep -v ref | head -1`
 echo UITestDLL: $UITestDLL
 
 UITestBuildDir=`dirname $UITestDLL`
